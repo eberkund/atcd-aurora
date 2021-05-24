1f6ec229fe1e0deb7246b399a17ad95601cf50ee
Erik Berkun-Drevnig <erik.b22@gmail.com>
2021-05-24 18:14:18 -0400

    Update coordinates

--- assets/scripts/map.ts
+++ assets/scripts/map.ts
@@ -2,8 +2,8 @@ let map: google.maps.Map;
 
 function initMap(): void {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: { lat: 43.99865398409355, lng: -79.47465612145655 },
+        zoom: 12,
         clickableIcons: false,
         zoomControl: false,
         mapTypeControl: false,
