4225962052508f5618eb459e6583378491e810f7
Erik Berkun-Drevnig <erik.b22@gmail.com>
2021-06-01 15:04:06 -0400

    Fix build

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,32 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//     content: ["./hugo_stats.json"],
+//     defaultExtractor: (content) => {
+//         const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+//         const innerMatches =
+//             content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+//         return broadMatches.concat(innerMatches);
+//     },
+// });
+
+// module.exports = {
+//     plugins: [
+//         require("postcss-import"),
+//         require("tailwindcss"),
+//         require("autoprefixer"),
+//         purgecss,
+//     ],
+// };
+
+let tailwindConfig =
+    process.env.HUGO_FILE_TAILWIND_CONFIG_JS || "./tailwind.config.js";
+const tailwind = require("tailwindcss")(tailwindConfig);
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
     plugins: [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        // purgecss({
-        //     content: ["./hugo_stats.json"],
-        //     defaultExtractor: (content) => {
-        //         let els = JSON.parse(content).htmlElements;
-        //         return els.tags.concat(els.classes, els.ids);
-        //     },
-        // }),
-        ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+        tailwind,
+        ...(process.env.HUGO_ENVIRONMENT === "production"
+            ? [autoprefixer]
+            : []),
     ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: process.env.HUGO_ENVIRONMENT === "production",
+        content: ["./hugo_stats.json", "./layouts/**/*.html"],
+        extractors: [
+            {
+                extractor: (content) => {
+                    let els = JSON.parse(content).htmlElements;
+                    return els.tags.concat(els.classes, els.ids);
+                },
+                extensions: ["json"],
+            },
+        ],
+        mode: "all",
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
