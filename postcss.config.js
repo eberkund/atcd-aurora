206d3e7c4ae4f180d03be09b7fe9358a684640ad
Erik Berkun-Drevnig <erik.b22@gmail.com>
2021-06-07 14:05:52 -0400

    Fix warning, add FB tracking code

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 const tailwind = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const atImport = require("postcss-import");
 
 module.exports = {
     plugins: [
         tailwind,
+        atImport,
         ...(process.env.HUGO_ENVIRONMENT === "production"
             ? [autoprefixer]
             : []),
