1db8d58a948636989b3d2c676aa861c6db820f92
Erik Berkun-Drevnig <erik.b22@gmail.com>
2021-06-07 12:30:52 -0400

    Misc updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,4 @@
-// const purgecss = require("@fullhuman/postcss-purgecss")({
-//     content: ["./hugo_stats.json"],
-//     defaultExtractor: (content) => {
-//         const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-//         const innerMatches =
-//             content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-//         return broadMatches.concat(innerMatches);
-//     },
-// });
-
-// module.exports = {
-//     plugins: [
-//         require("postcss-import"),
-//         require("tailwindcss"),
-//         require("autoprefixer"),
-//         purgecss,
-//     ],
-// };
-
-let tailwindConfig = "./tailwind.config.js";
-const tailwind = require("tailwindcss")(tailwindConfig);
+const tailwind = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
