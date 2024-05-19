63bccf6e016511a0213ddb2cc1cb7e73f8d5852d
Erik Berkun-Drevnig <erik.b22@gmail.com>
2024-05-19 17:18:25 -0400

    Use native datepicker and update libraries

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,2 @@
 import "alpinejs";
 import "scripts/pixel";
-import Datepicker from "flowbite-datepicker/Datepicker";
-
-const datepickerEl = document.querySelector("[datepicker]");
-new Datepicker(datepickerEl, {
-    autohide: true,
-});
