29e7f895d7b5e42155cbff77cd38763ce2bb882f
Erik Berkun-Drevnig <erik.b22@gmail.com>
2024-05-20 04:10:35 -0400

    Adjust colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ import Color from "color";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    ...(process.env.HUGO_ENVIRONMENT == "production" && {
-        safelist: [{ pattern: /.*/ }],
-    }),
+    // ...(process.env.HUGO_ENVIRONMENT == "production" && {
+    //     safelist: [{ pattern: /.*/ }],
+    // }),
+    safelist: [{ pattern: /.*/ }],
     purge: {
         content: ["./hugo_stats.json", "./layouts/**/*.html"],
         extractors: [
@@ -22,27 +23,48 @@ module.exports = {
             typography: {},
             colors: {
                 navy: {
-                    400: Color("#1b75bc").hex(),
+                    100: Color("#1b75bc").lighten(0.4).hex(),
+                    200: Color("#1b75bc").lighten(0.3).hex(),
+                    300: Color("#1b75bc").lighten(0.2).hex(),
+                    400: Color("#1b75bc").lighten(0.1).hex(),
+                    500: Color("#1b75bc").hex(),
+                    600: Color("#1b75bc").darken(0.1).hex(),
+                    700: Color("#1b75bc").darken(0.2).hex(),
+                    800: Color("#1b75bc").darken(0.3).hex(),
+                    900: Color("#1b75bc").darken(0.4).hex(),
                 },
-                mauve: {
-                    400: Color("#69385c").hex(),
-                    600: Color("#e4c1f9").darken(0.2).hex(),
-                },
-                mint: {
-                    400: Color("#f1fffa").hex(),
-                    600: Color("#f1fffa").darken(0.2).hex(),
-                    600: Color("#f1fffa").darken(0.3).hex(),
-                    800: Color("#f1fffa").darken(0.8).hex(),
+                amber: {
+                    100: Color("#ffbe00").lighten(0.4).hex(),
+                    200: Color("#ffbe00").lighten(0.3).hex(),
+                    300: Color("#ffbe00").lighten(0.2).hex(),
+                    400: Color("#ffbe00").lighten(0.1).hex(),
+                    500: Color("#ffbe00").hex(),
+                    600: Color("#ffbe00").darken(0.1).hex(),
+                    700: Color("#ffbe00").darken(0.2).hex(),
+                    800: Color("#ffbe00").darken(0.3).hex(),
+                    900: Color("#ffbe00").darken(0.4).hex(),
                 },
                 coral: {
-                    300: Color("#ef767a").lighten(0.1).hex(),
+                    100: Color("#ef767a").lighten(0.4).hex(),
+                    200: Color("#ef767a").lighten(0.3).hex(),
+                    300: Color("#ef767a").lighten(0.2).hex(),
+                    400: Color("#ef767a").lighten(0.1).hex(),
                     500: Color("#ef767a").hex(),
-                    700: Color("#ef767a").darken(0.1).hex(),
+                    600: Color("#ef767a").darken(0.1).hex(),
+                    700: Color("#ef767a").darken(0.2).hex(),
+                    800: Color("#ef767a").darken(0.3).hex(),
+                    900: Color("#ef767a").darken(0.4).hex(),
                 },
                 keppel: {
-                    300: Color("#49beaa").lighten(0.1).hex(),
+                    100: Color("#49beaa").lighten(0.8).hex(),
+                    200: Color("#49beaa").lighten(0.6).hex(),
+                    300: Color("#49beaa").lighten(0.4).hex(),
+                    400: Color("#49beaa").lighten(0.2).hex(),
                     500: Color("#49beaa").hex(),
-                    700: Color("#49beaa").darken(0.1).hex(),
+                    600: Color("#49beaa").darken(0.1).hex(),
+                    700: Color("#49beaa").darken(0.2).hex(),
+                    800: Color("#49beaa").darken(0.3).hex(),
+                    900: Color("#49beaa").darken(0.4).hex(),
                 },
             },
         },
